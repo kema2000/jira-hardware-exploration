abc69aa4dab5aecf8073124fe89c6fc2ed955030
Kevin Ma <kma2@atlassian.com>
2019-04-08 13:30:19 +1000

    Changed task name

--- src/test/kotlin/com/atlassian/performance/tools/hardware/IntegrationTestRuntime.kt
+++ src/test/kotlin/com/atlassian/performance/tools/hardware/IntegrationTestRuntime.kt
@@ -17,7 +17,7 @@ import java.util.*
 
 object IntegrationTestRuntime {
 
-    const val taskName = "QUICK-103"
+    const val taskName = "QUICK-103-CI"
     val workspace = RootWorkspace(Paths.get("build")).isolateTask(taskName)
 
     val aws: Aws
